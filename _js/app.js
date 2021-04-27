9cfc1582ad604e59953196a9521a59e2f7f0da96
RODRIGO MONTEBELLO SABOYA BRITO <blackknight@blackknightstudio.com.br>
2021-04-27 12:02:19 -0300

    Aulas 1 e 2
    Detalhes sobre js e variáveis

---
+++ _js/app.js
@@ -0,0 +1,11 @@
+//O comando console.log exibe mensagens no console JS dos navegadores.
+Console.log("Olá mundo");
+//Em javaScript o tipo da variável é definido pela atribuição.
+var numero = 100; //isto é um inteiro.
+var string = 'isto é uma string';
+var float = 100.34; //isto é um float;
+var boolean = true; //isto é um boolean (verdadeiro e falso)
+var indefinido; //esta variável tem valor de indefinido(undefined) pois nada foi associado a ela.
+var nulo = null; //está variável é considerada nula
+const fixa = 100; //Uma vez que seu valor foi atribuido está variável não pdoe ser alterada.
+// 'var' pode ser declarado mais de uma vez criando bugs no código, enquanto const não pdoe ser declarado novamente.
